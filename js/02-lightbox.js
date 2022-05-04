c45e2b089c65502e02c61df8797c165017743155
MaxVidom <MaxVidomenko@gmail.com>
2022-05-04 07:25:05 +0300

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galeryUlRef = document.querySelector('ul.gallery');
+
+function createGalery(items) {
+    const galeryMarkup = items.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description} title="${description}" />
+</a>`;
+    }).join("");
+    galeryUlRef.insertAdjacentHTML("afterbegin", galeryMarkup);
+}
+createGalery(galleryItems);
+
+let gallery = new SimpleLightbox('ul.gallery a');
+gallery.on(`show.simplelightbox`, function () {
+    galery.defaultOptions.captionDelay = 250;
+});
